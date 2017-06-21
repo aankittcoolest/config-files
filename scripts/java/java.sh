3ed57124d625b8efca39494c0090b78be24c4b6b
aankittcoolest <aankittcoolest@gmail.com>
2017-06-21 13:02:06 +0000

    Adding some java scripts

---
+++ scripts/java/java.sh
@@ -0,0 +1,3 @@
+sh find_java.sh
+sudo yum -y localinstall jdk-8u131-linux-x64.rpm
+rm jdk-8u112-8u131-linux-x64.rpm
---
+++ scripts/powerline.sh
@@ -0,0 +1,10 @@
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+rpm -ivh epel-release-7-9.noarch.rpm
+yum -y install python-pip
+rm -rf epel-release-7-9.noarch.rpm
+pip install git+git://github.com/Lokaltog/powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf /usr/share/fonts/
+fc-cache -vf /usr/share/fonts/
+mv 10-powerline-symbols.conf /etc/fonts/conf.d/
