e724422370bc2b8626d1c6027dda97b6d50431d9
Ankit Agrawal <aankittcoolest@gmail.com>
2018-12-11 16:39:29 +0900

    Add calendar settings

--- settings/emacs/init.el
+++ settings/emacs/init.el
@@ -18,21 +18,5 @@
 
 
 (org-babel-load-file (expand-file-name "~/config-files/settings/emacs/myinit.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (mu4e-alert org-mime projectile atomic-chrome ox-reveal zenburn-theme which-key use-package try org-pdfview org-bullets magit evil auto-complete ace-window ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
-
-
-
+(put 'dired-find-alternate-file 'disabled nil)
