125f9d11d022d20dc216f803a0bf989fde1e878c
ankit.agrawal <ankit.agrawal@rakuten.com>
2021-12-17 23:00:15 +0530

    Adding more emacs configuration

--- settings/emacs/init.el
+++ settings/emacs/init.el
@@ -20,3 +20,18 @@
 (org-babel-load-file (expand-file-name "~/config-files/settings/emacs/myinit.org"))
 
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(package-selected-packages
+   '(urlenc json-mode markdown-toc yasnippet-snippets yasnippet company-restclient restclient load-theme-buffer-local slack websocket request circe ox-jira magit multi-term emojify projectile ox-reveal all-the-icons powerline dired-hacks-utils unicode-fonts auto-dictionary auto-complete key-chord neotree evil-magit evil ace-jump-mode ace-window htmlize org-bullets which-key try use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
